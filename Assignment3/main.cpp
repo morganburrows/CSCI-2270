15a8c6d3e5fd5406c1c98580bd326ad7b0ef5263
morganburrows <burrowmt@gmail.com>
2016-02-06 18:06:34 -0700

    exit case online

---
+++ Assignment3/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    //  begin application, output menu
+
+int minput;
+cout<<"======Main Menu======"<<endl;;
+cout<<"1. Build Network"<<endl;
+cout<<"2. Print Network Path"<<endl;
+cout<<"3. Transmit Message Coast-To-Coast"<< endl;
+cout<<"4. Add City"<<endl;
+cout<<"5. Quit"<<endl;
+
+cin >> minput;
+
+switch(minput)  //  main switch for menu input
+{
+    case 1:
+
+    case 2:
+
+    case 3:
+
+    case 4:
+
+    case 5: exit(0);
+}
+
+
+
+}   //  end of main
